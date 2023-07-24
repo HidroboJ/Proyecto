bb583079e0c722c3b3f10ebe24613086a191df26
johan <johan@DESKTOP-359EB2T>
2023-07-24 11:28:55 -0500

    Agregar archivos de proyecto.

---
+++ ProyectoHotel/Logica/Conexion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoHotel.Logica
+{
+    public class Conexion
+    {
+        public static string CN = "Data Source=DESKTOP-359EB2T\\MSSQLSERVER2;Initial Catalog=DB_HOTEL;Integrated Security=True";
+    }
+}
